ebd40bacd6157c47d8f080ba826a3a730bfd8cd3
JenselJ <saneer2002@gmail.com>
2022-08-28 08:31:35 +1000

    just before adding tailwind prefix

--- src/Components/home-page.js
+++ src/Components/home-page.js
@@ -9,45 +9,63 @@ export default function Home(props) {
   const navigate = useNavigate()
 
   return (
-    <div className="container">
-      <div className="d-flex">
-        <h1 className = "selective-entry-title">Sunny's Spectacular Quizes</h1>
-        <div className="my-auto">
-          <Button className="mx-2" onClick={()=>{navigate('/login')}}>
-            Login
-          </Button>
+    <div className="min-h-screen flex flex-col text-white">
+      <main className="container mx-auto px-6 pt-16 flex-1 text-center">
+
+        <h2 className="text-2xl md:text-4xl lg:text-6xl uppercase">
+          Welcome to
+        </h2>
+
+        <h1 className="text-3xl md:text-6xl lg:text-8xl font-black uppercase mb-8">
+          The quiz club
+        </h1>
+
+        <div className="text-lg md:text-2xl lg:text-3xl py-2 px-4 md:py-4 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-10 w-fit mx-auto mb-8 rounded-full">
+          942,243 members
         </div>
-        
 
-      </div>
+        <form>
+          <div className="flex flex-col md:flex-row justify-center mb-4">
+            <input 
+              className="text-lg md:text-2xl placeholder:text-gray-400 placeholder:italic py-4 px-6 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-10 focus:bg-opacity-20 duration-150 rounded-full md:rounded-tr-none md:rounded-br-none mb-4 md:mb-0 outline-none"
+              placeholder="Your email ..."
+              type="email"
+              name="member[email]"
+              />
+            <input 
+              type="submit"
+              value="Join today"
+              name="memeber[subscribe]"
+              className="bg-primary rounded-full md:rounded-tl-none md:rounded-bl-none text-lg md:text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 cursor-pointer hover:opacity-75 duration-150"
+              onClick={()=> {navigate('/signup')}}
+            />
+          </div>
+
+          <div className="opacity-75 italic">
+          Already a member? <a className="cursor-pointer hover:opacity-75 duration-150 underline" onClick={()=>{navigate('/login')}}>Log in</a>
+        </div>
+
+        </form>
+
+
+      </main>
+
+      <footer className="container mx-auto p-6 flex flex-col md:flex-row items-center justify-between">
+        <p>
+          Built with love by Sunny!
+        </p>
+
+        <div className="flex -mx-6">
+          <a href="#" className="mx-3 hover:opacity-80 duration-150">About us</a> |
+          <a href="#" className="mx-3 hover:opacity-80 duration-150">Privacy</a> |
+          <a href="#" className="mx-3 hover:opacity-80 duration-150">Contact</a>
+        </div>
+
+      </footer>
+
+    </div>
     
-    <div className ='div0'>
-      <img style={{maxWidth: "100%"}} src='study.png' alt='students learning' height='403' class='center'/>
-    <p className="quote">Test your general and front-end knowledge!
-        <br></br><button style={{marginTop: "20px"}} onClick={()=> {navigate('/signup')}} class="join-button">Join Now</button>
-    </p>
-    </div>
-    {/* <div className = "div-one">
-      <h2>Thinking Skills package from just $10</h2>
-        <ul>
-      <li>100+ Questions</li>
-      <li>3 Full Mock Exams</li>
-      <li>In-Depth Explanations for all questions</li>
-      <li>Structured Learning</li>
-      <li>Updated Weekly</li>
-      </ul> 
-      <div className = "learn-more-div">
-        <button className ="learn-more-button">Learn More</button>
-      </div>
-    </div>
-
-    <div className = "div-two">
-      <div className = "sample-test-div">
-      <a href="/login"><button className="sample-test-button">Take free Thinking Skills sample test</button></a>
-      </div>
-    </div> */}
-
-    </div>
   );
 }
+
 
